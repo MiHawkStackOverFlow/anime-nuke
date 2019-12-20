0efce48642b54d1d9290dfcca4abe69e7d0faa57
Abhishek Sharma <abhivsharma8@gmail.com>
2019-12-20 14:41:18 +0530

    dashboard children

--- src/app/modules/dashboard/dashboard-routing.module.ts
+++ src/app/modules/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { GamesListComponent } from './components/games-list/games-list.component';
 
 const routes: Routes = [
   { 
     path: '', component: DashboardComponent,
     children : [
-       
+      { path: 'users-list', component: UserListComponent },
+      { path: 'games-list', component: GamesListComponent }
     ] 
   }
 ];
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { AppMaterialModule } from './../../app-material/app-material.module';
 import { DashboardComponent } from './dashboard.component';
-
+import { UserListComponent } from './components/user-list/user-list.component';
+import { GamesListComponent } from './components/games-list/games-list.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, UserListComponent, GamesListComponent],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    AppMaterialModule
   ]
 })
 export class DashboardModule { }
