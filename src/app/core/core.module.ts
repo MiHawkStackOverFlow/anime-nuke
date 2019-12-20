a674bef905cbd1d2b588195ed0bede39596d87a6
Abhishek Sharma <abhivsharma8@gmail.com>
2019-12-20 14:16:39 +0530

    seperation of modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppMaterialModule } from './../modules/app-material/app-material.module';
+import { AppMaterialModule } from '../app-material/app-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginComponent } from './components/login/login.component';
 import { TopNavComponent } from './components/top-nav/top-nav.component';
