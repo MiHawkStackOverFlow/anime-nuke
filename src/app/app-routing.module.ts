588ee32c327d1b46b5b3a7e885654b7007333c99
Abhishek Sharma <abhivsharma8@gmail.com>
2020-01-09 15:19:15 +0530

    Authorization per role

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/helpers/auth.guard';
-
+import { Role } from './core/models/role';
 import { LoginComponent } from './core/components/login/login.component';
 import { NotFoundComponent } from './shared/components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent }, 
-  { path: 'accounts', 
+  { 
+    path: 'accounts', 
     loadChildren: () => import('./modules/accounts/accounts.module').then(m => m.AccountsModule), 
-    canActivate: [AuthGuard] 
+    canActivate: [AuthGuard],
+    data: { roles: [Role.Admin] } 
   }, 
-  { path: 'dashboard', 
+  { 
+    path: 'dashboard', 
     loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule), 
-    canActivate: [AuthGuard] 
+    canActivate: [AuthGuard]
   },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '404', component: NotFoundComponent, canActivate: [AuthGuard] },
