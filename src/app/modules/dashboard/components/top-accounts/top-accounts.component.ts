aa2e9c88d6db9ec4f0616919a032451cf982702a
Abhishek Sharma <abhivsharma8@gmail.com>
2020-01-29 16:00:03 +0530

    rxjs-1
    Create an observable and pass a subscriber

--- src/app/modules/dashboard/components/top-accounts/top-accounts.component.ts
+++ src/app/modules/dashboard/components/top-accounts/top-accounts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Observable, Subscriber } from 'rxjs';
+import { topAccounts } from '../../models/data';
 
 @Component({
   selector: 'app-top-accounts',
@@ -10,10 +11,19 @@ export class TopAccountsComponent implements OnInit {
 
   constructor() { }
 
-  
+  subscribe(subscriber: Subscriber<any>): void {
+    for (let account of topAccounts) {
+      subscriber.next(account);
+    }
+  }
 
   ngOnInit() {
-   
+    let topAccountsObservable$ = new Observable(subscriber => {
+      for (let account of topAccounts) {
+        subscriber.next(account);
+      }
+    });
+    topAccountsObservable$.subscribe((account: any) => console.log("Account Name", account.name));
   }
 
 }
